2fb3c9f779133b36c0d97bf60efac522e076032f
Stark-Akshay <akshaymanoj.dms@gmail.com>
2024-11-08 23:17:29 +0000

    added a redirect for wrong ign

--- app/obsComponent/[slug]/page.js
+++ app/obsComponent/[slug]/page.js
@@ -1,9 +1,14 @@
 import { singlePersonFetch } from "@/app/utils/singlePersonFetch";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }) {
   const slug = (await params).slug;
   const result = await singlePersonFetch(slug);
-  console.log(result.data[0].rank);
+  if (!result.data[0]) {
+    redirect("/");
+  }
+
+  console.log(result);
   return (
     <div className="w-full h-full flex flex-row justify-center items-center bg-transparent">
       <h2 className="text-finalsyellow text-4xl font-bold">
