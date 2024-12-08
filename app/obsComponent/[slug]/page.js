b43772e16d2278397ffecd58f87ee8aa1654d3ae
Stark-Akshay <akshaymanoj.dms@gmail.com>
2024-12-08 13:34:03 +0000

    WT component revalidation v1

--- app/obsComponent/[slug]/page.js
+++ app/obsComponent/[slug]/page.js
@@ -1,12 +1,37 @@
+// import { singlePersonFetch } from "@/app/utils/singlePersonFetch";
+// import { redirect } from "next/navigation";
+
+// export default async function Page({ params }) {
+//   const slug = (await params).slug;
+//   const result = await singlePersonFetch(slug);
+//   if (!result.data[0]) {
+//     redirect("/");
+//   }
+//   return (
+//     <div className="w-full h-full flex flex-row justify-center items-center bg-transparent">
+//       <h2 className="text-finalsyellow text-9xl font-bold">
+//         Rank: #{result.data[0].rank}
+//       </h2>
+//     </div>
+//   );
+// }
+
 import { singlePersonFetch } from "@/app/utils/singlePersonFetch";
 import { redirect } from "next/navigation";
 
+export const revalidate = 1800; // Revalidate every 30 minutes
+
 export default async function Page({ params }) {
   const slug = (await params).slug;
+
+  // Fetch data
   const result = await singlePersonFetch(slug);
-  if (!result.data[0]) {
-    redirect("/");
+
+  if (!result.data || !result.data[0]) {
+    redirect("/"); // Redirect to home if no data found
   }
+
+  // Render page
   return (
     <div className="w-full h-full flex flex-row justify-center items-center bg-transparent">
       <h2 className="text-finalsyellow text-9xl font-bold">
