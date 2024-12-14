03905abc909454af6f9cc1fe92678284ff002b63
Stark-Akshay <akshaymanoj.dms@gmail.com>
2024-12-14 20:07:35 +0000

    season 5 ranked

--- app/utils/singlePersonFetchRanked.js
+++ app/utils/singlePersonFetchRanked.js
@@ -12,7 +12,7 @@
 
 export async function singlePersonFetchRanked(slug, options = {}) {
   const res = await fetch(
-    `https://api.the-finals-leaderboard.com/v1/leaderboard/s4/crossplay?name=${slug}`,
+    `https://api.the-finals-leaderboard.com/v1/leaderboard/s5/crossplay?name=${slug}`,
     options
   );
   const data = await res.json();
